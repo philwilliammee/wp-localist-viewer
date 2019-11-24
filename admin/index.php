8c11af536eea4a29d138adda354c0dbd9374b942
philwilliammee <philliammee@yahoo.com>
2019-11-24 07:32:04 -0500

    more phpcs

--- admin/index.php
+++ admin/index.php
@@ -1,1 +1,16 @@
-<?php // Silence is golden
+<?php
+/**
+ * The index file.
+ *
+ * PHP version 7^.0
+ *
+ * @category   Events
+ * @package    Cwd_Events
+ * @subpackage Cwd_Events/includes
+ * @author     philwilliammee <psw58@cornell.edu>
+ * @license    gpl3.0 https://www.gnu.org/licenses/gpl-3.0.en.html
+ * @link       philwilliammee.com
+ * @since      1.0.0
+ */
+
+// Silence is golden.
